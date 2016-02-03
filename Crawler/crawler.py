bb894696e20b8fdbaf5e16238abbc76b1d958886
olgaramz <olga.ramz.2012@gmail.com>
2016-02-03 16:35:48 +0300

    Update crawler.py

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -26,27 +26,34 @@ for i in queue:
             html1 = page1.read().decode()
             tree1 = lxml.html.fromstring(html1)
             title1 = tree1.findtext('.//title')
+            #author1 = tree1.xpath('.//a[@rel="author"]/text()')[0]
             visited.append(i)
             queue.remove(i)
-            f = open(title + '.txt', 'w', encoding = 'utf-8') #invalid syntax?
+            f = open(title1[1:5] + '.txt', 'w', encoding = 'utf-8') #filename
             f.write(html1)
             f.close()
             linksOnPage = tree1.xpath('.//a/@href')
             for n in linksOnPage:
                 M = re.search('/index\.php/[^"]*', n)
                 if M != None:
-                    queue.append(baseurl + M.group(0)
-    elif i in visited: #invalid syntax
-        queue.remove(i)
+                    queue.append(baseurl + M.group(0))
 
 #for one page
 
-pageurl = 'http://www.gazetayakutia.ru/index.php/component/k2/item/19844-dmitrij-glushko-ya-obyazatelno-vernus-v-yakutiyu'
 
-with urllib.request.urlopen('http://www.gazetayakutia.ru/index.php/component/k2/item/19844-dmitrij-glushko-ya-obyazatelno-vernus-v-yakutiyu') as page:
+with urllib.request.urlopen('http://www.gazetayakutia.ru/index.php/component/k2/item/19959-chzhoda-poluchila-prava-zastrojshchika') as page:
    pagehtml = page.read().decode()
 
 pagetree = lxml.html.fromstring(pagehtml)
 title = pagetree.findtext('.//title')
-author = pagetree.xpath('.//li[@style="padding:0 4px 0 0;"]/text()')#[0] #empty
-text = pagetree.findtext('.//!-- Item fulltext --/div[@class="itemFullText"]') #empty
+author = pagetree.xpath('.//a[@rel="author"]/text()')[0]
+text = pagetree.xpath('.//div[@class="itemFullText"]/p/text()') 
+date = pagetree.xpath('.//li/text()')[7]
+introtext = pagetree.findtext('.//div[@class="itemIntroText"]/p')
+
+#saving files to correct directories
+
+#os.makedirs()
+
+#filename = title[1:5] + '.txt'
+#os.path.join(date, filename)
