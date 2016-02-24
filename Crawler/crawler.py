479f0f46c39af98af5652f989c03644b5841a770
olgaramz <olga.ramz.2012@gmail.com>
2016-02-24 11:51:55 +0300

    Update crawler.py

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -19,72 +19,55 @@ for i in links:
 
 for i in queue:
     if i not in visited:
+        counter += 1
         try:
             with urllib.request.urlopen(i) as page:
                 pagehtml = page.read().decode()
-            pagetree = lxml.html.fromstring(pagehtml)
-            title = pagetree.findtext('.//title')
-            try:            
-                author = pagetree.xpath('.//a[@rel="author"]/text()')[0]
-            except IndexError:
-                author = 'no author'
-            try:
-                date = pagetree.xpath('.//li/text()')[7]
-                date = date.replace('\r', '')
-                date = date.replace('\n', '')
-                date = date.replace('\t', '')
-                date = date.replace(':', '')
-                if date == '':
-                    date = 'unknown date'
-            except IndexError:
-                date = 'unknown date'
-            introtext = pagetree.findtext('.//div[@class="itemIntroText"]/p')
-            text = pagetree.xpath('.//div[@class="itemFullText"]/p/text()')
-            textstring = '\n'.join(text)
-            linksOnPage = pagetree.xpath('.//a/@href')
-            for n in linksOnPage:
-                M = re.search('/index\.php/[^"]*', n)
-                if M != None:
-                    queue.append(baseurl + M.group(0))
+                pageurl = i    
         except UnicodeEncodeError:
             m = re.match('http://www.gazetayakutia.ru/index.php/component/k2/itemlist/tag/(.*)', i)
             if m != None:
                 try:
                     with urllib.request.urlopen('http://www.gazetayakutia.ru/index.php/component/k2/itemlist/tag/' + urllib.request.quote(m.group(1))) as page:
                         pagehtml = page.read().decode()
-                except urllib.request.HTTPError:
-                    pass
-            pagetree = lxml.html.fromstring(html)
-            title = tree.findtext('.//title')
-            try:            
-                author = pagetree.xpath('.//a[@rel="author"]/text()')[0]
-            except IndexError:
-                author = 'no author'
-            try:
-                date = pagetree.xpath('.//li/text()')[7]
-                date = date.replace('\r', '')
-                date = date.replace('\n', '')
-                date = date.replace('\t', '')
-                date = date.replace(':', '')
-                if date == '':
-                    date = 'unknown date'
-            except IndexError:
-                date = 'unknown date'
-            introtext = pagetree.findtext('.//div[@class="itemIntroText"]/p')
-            text = pagetree.xpath('.//div[@class="itemFullText"]/p/text()')
-            textstring = '\n'.join(text)
-            linksOnPage = pagetree.xpath('.//a/@href')
-            for n in linksOnPage:
-                M = re.search('/index\.php/[^"]*', n)
-                if M != None:
-                    queue.append(baseurl + M.group(0))
-        f = open(date + '.html', 'w', encoding = 'utf-8') #filename
-        f.write(pagehtml)
-        f.close()
-        f1 = open(date + '.txt', 'w', encoding = 'utf-8')
-        f1.write(str(author) + '\n' + date + '\n' + str(introtext) + '\n' + textstring)
+                        pageurl = i
+                except urllib.error.HTTPError:
+                    continue       
+        pagetree = lxml.html.fromstring(pagehtml)
+        title = pagetree.findtext('.//title')
+        try:            
+            author = pagetree.xpath('.//a[@rel="author"]/text()')[0]
+        except IndexError:
+            author = 'Noname'
+        date1 = pagetree.xpath('.//li/text()')
+        for x in date1:
+            p = re.search('\.*([A-z]*, [0-9]{2} [A-z]* [0-9]{4} [0-9]{2}:[0-9]{2})', x)
+            if p != None:
+                dateCreated = p.group(1)
+                dateCreated = dateCreated.replace(':', '_')
+        introtext = pagetree.findtext('.//div[@class="itemIntroText"]/p')
+        text = pagetree.xpath('.//div[@class="itemFullText"]/p/text()')
+        textstring = '\n'.join(text)
+        linksOnPage = pagetree.xpath('.//a/@href')
+        for n in linksOnPage:
+            M = re.search('/index\.php/[^"]*', n)
+            if M != None:
+                queue.append(baseurl + M.group(0))
+        if dateCreated != 'unknown date':
+            splitDate = dateCreated.split()
+            year = splitDate[3]
+            month = splitDate[2]
+            day = splitDate[1]
+        else:
+            year = 'unknown year'
+            month = 'unknown month'
+            day = 'unknown day'
+        path = 'C:\\Users\\User\\Documents\\Corpus\\plaintext\\' + year + '\\' + month
+        filename = path + '\\' + dateCreated + str(counter) + '.txt'
+        if os.path.exists(path) is False:
+            os.makedirs(path)
+        f1 = open(filename, 'w', encoding = 'utf-8')
+        f1.write('@au ' + author + '\n' + '@ti ' + title + '\n' + '@da ' + dateCreated + '\n' + str(introtext) + '\n' + textstring)
         f1.close()            
         visited.append(i)
-        queue.remove(i)
-    else:
-        queue.remove(i)
+        print(counter) #remove later
