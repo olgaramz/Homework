7427c97620c59d01c445adaf0c60450f361ec5bd
olgaramz <olga.ramz.2012@gmail.com>
2016-06-20 00:58:26 +0400

    Update converter.py

--- xml-prs-converter/converter.py
+++ xml-prs-converter/converter.py
@@ -1,7 +1,5 @@
-import sys
 import re
 import argparse
-
 
 def prstoxml(filename):
     xmltext = '<body>\n'
@@ -17,18 +15,10 @@ def prstoxml(filename):
         if str(i) not in seen:
             seen.add(str(i))        
             currentstrng = ''
-            seNum = i[0]
-            wordNum = i[1]
-            lang = i[2]
-            wordGraph = i[3]
             word = i[4]
-            indexword = i[5]
             nvars = i[6] #number of variants
-            nlems = i[7] #number of lemmas
-            nvar = i[8]
             lem = i[9]
             trans = i[10]
-            transRu = i[11]
             lex = i[12]
             gram = i[13]
             flex = i[14]
@@ -46,8 +36,7 @@ def prstoxml(filename):
                 if variants > 1:
                     tolookahead = listOfColumns[index:index+variants]
                     for j in tolookahead:
-                        trans1 = j[10]                        
-                        transRu1 = j[11]
+                        trans1 = j[10]
                         lem1 = j[9]
                         lex1 = j[12]
                         flex1 = j[14]
@@ -66,17 +55,58 @@ def prstoxml(filename):
     xmltext += '</body>'
     return xmltext   
 
-
+def xmltoprs(filename):
+    fulltext = []
+    lines = ''
+    prstxt = '#sentnum\t#wordnum\t#graphword\t#numvar\t#lex\t#morph\t#gram\t#trans\n'
+    with open(filename, 'r', encoding='utf-8') as text:
+        for line in text:
+            newline = line.replace('\n', '')
+            lines += newline
+    fulltext = re.split('(?<=</se>)\s+(?=<se>)', lines)
+    for index, value in enumerate(fulltext):
+        sentnum = str(index + 1) #1
+        words = re.split('(?<=</w>)\s+(?=<w>)', value)
+        for index, value in enumerate(words):
+            wordnum = str(index + 1) #2
+            g = re.search('(?<=/>)\s+\w+\s+(?=</w>)', value)
+            if g != None:
+                graphword = g.group(0)
+            graphword = graphword.replace(' ', '') #3
+            annotations = re.findall('<ana.+?/>', value)
+            for index, value in enumerate(annotations):
+                numvar = index + 1
+                numberVar = str(numvar)
+                a = re.search('lex="(\w+)"', value)
+                lex = a.group(1) #4
+                m = re.search('morph="(.*?)"', value)
+                if m == None:
+                    morph = ''
+                else:
+                    morph = m.group(1) #5
+                gr = re.search('gr="(.*?)"', value)
+                gram = gr.group(1) #6
+                t = re.search('trans="(.*?)"',value)
+                trans = t.group(1) #7
+                finalstrng = sentnum + '\t' + wordnum + '\t' + graphword + '\t' + numberVar + '\t' + lex + '\t' + morph + '\t' + gram + '\t' + trans + '\n'
+                prstxt += finalstrng
+    return prstxt
+                
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input', required=True)
 parser.add_argument('-o', '--output', required=True)
-parser.add_argument('-p', '--prs')
+parser.add_argument('-f', '--format', required=True)
 args = parser.parse_args()
 
 inputfile = args.i
 outputfile = args.o
 
-if args.prs:
+if args.format == 'prs':
     m = prstoxml(inputfile)
+    with open(outputfile, 'w', encoding='utf-8') as f:
+        f.write(m)
+        
+if args.format == 'xml':
+    m = xmltoprs(inputfile)
     with open(outputfile, 'w', encoding='utf-8') as f:
         f.write(m)
